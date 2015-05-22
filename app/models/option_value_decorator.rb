2b8b385c24769abb3cee100a566635e94c8e4fcf
Matteo Latini <mtylty@gmail.com>
2015-05-22 15:53:16 +0200

    adds ugly fix to make migrate work

--- app/models/option_value_decorator.rb
+++ app/models/option_value_decorator.rb
@@ -1,6 +1,6 @@
 OptionValue.class_eval do
 
-  default_scope order(:position)
+  default_scope order(:position) if table_exists?(:option_values)
     
   if defined?(SpreeHeroku)
     has_attached_file :image, 
